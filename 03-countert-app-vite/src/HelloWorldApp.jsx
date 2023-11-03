7009315b5cb0adc82f56448d68c52517bdc8d4ea
batishavo <romero.martinez.carlosraul@gmail.com>
2023-11-02 19:19:10 -0600

    avance del curso

---
+++ 03-countert-app-vite/src/HelloWorldApp.jsx
@@ -0,0 +1,13 @@
+// function App(){
+//     // document crear
+//     return (<h1>Hola mundo</h1>);
+// }
+
+// export default App;
+// import React from 'react'
+
+export const HelloWorldApp = () => {
+  return (
+    <h1>HelloWorldApp</h1>
+  )
+}
